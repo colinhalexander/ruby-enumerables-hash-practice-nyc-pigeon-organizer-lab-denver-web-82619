669e664dd23745485941a1f4cae028842f0cc338
Colin Alexander <chalexander@me.com>
2019-08-24 17:34:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final_pigeon_data = data.reduce({}) do |memo, (key, value)|
+    
+    value.reduce({}) do |inner_memo, (inner_key, inner_value)|
+      
+      count = 0
+      while count < inner_value.length do
+        if (!memo[inner_value[count]])
+          memo[inner_value[count]] = {
+            color: []
+            gender: []
+            lives: []
+          }
+        end
+        
+        count += 1
+      end
+    end
+  end
 end
